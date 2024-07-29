7aade833882aeb88ea08c0ab1d4fc671159b8a08
Moyosoreoluwa <dev.muyiwa@gmail.com>
2024-07-29 17:59:04 +0100

    Refactored the database module.

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,13 +1,6 @@
-import { Global, Inject, Module, OnModuleInit } from '@nestjs/common';
-import { knex, Knex } from 'knex';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseService } from './database.service';
-import { getKnexConfig } from './knexfile';
-import { Logger } from 'winston';
-import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
-import Postgrator from 'postgrator';
-import * as os from 'node:os';
-import * as process from 'node:process';
 
 @Global()
 @Module({
@@ -16,90 +9,13 @@ import * as process from 'node:process';
     DatabaseService,
     {
       provide: 'KnexConnection',
-      useFactory: async (
-        configService: ConfigService,
-        logger: Logger,
-      ): Promise<Knex> => {
-        const dbConfig = getKnexConfig(configService);
-        const connection = knex(dbConfig);
-
-        try {
-          await connection.raw('SELECT 1+1 AS result');
-          logger.info('Database connection established successfully', {
-            context: 'DatabaseModule',
-          });
-
-          connection.on('error', (err) =>
-            logger.error('Database error', {
-              error: err,
-              context: 'DatabaseModule',
-            }),
-          );
-
-          return connection;
-        } catch (error) {
-          logger.error('Failed to connect to the database', {
-            error,
-            context: 'DatabaseModule',
-          });
-          throw error;
-        }
+      useFactory: (databaseService: DatabaseService) => {
+        return databaseService.getKnexConnection();
       },
-      inject: [ConfigService, WINSTON_MODULE_PROVIDER],
+      inject: [DatabaseService],
     },
   ],
   exports: ['KnexConnection', DatabaseService],
 })
-export class DatabaseModule implements OnModuleInit {
-  constructor(
-    private readonly configService: ConfigService,
-    @Inject('KnexConnection') private readonly knexConnection: Knex,
-    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
-  ) {}
-
-  async onModuleInit() {
-    const dbConfig = getKnexConfig(this.configService);
-
-    const postgrator = new Postgrator({
-      migrationPattern: `${process.cwd()}/migrations/*`,
-      driver: 'pg',
-      database: (dbConfig.connection as any).database,
-      schemaTable: 'schema_migrations',
-      execQuery: (query) => this.knexConnection.raw(query),
-    });
-
-    postgrator.on('validation-started', (migration) =>
-      this.logger.info(`Validating ${migration.filename}`, {
-        context: 'DatabaseModule',
-      }),
-    );
-    postgrator.on('validation-finished', (migration) =>
-      this.logger.info(`Validated ${migration.filename}`, {
-        context: 'DatabaseModule',
-      }),
-    );
-    postgrator.on('migration-started', (migration) =>
-      this.logger.info(`Migrating ${migration.filename}`, {
-        context: 'DatabaseModule',
-      }),
-    );
-    postgrator.on('migration-finished', (migration) =>
-      this.logger.info(`Migrated ${migration.filename}`, {
-        context: 'DatabaseModule',
-      }),
-    );
-
-    try {
-      const migrations = await postgrator.migrate();
-      this.logger.info(`Ran ${migrations.length} migrations`, {
-        context: 'DatabaseModule',
-      });
-    } catch (error) {
-      this.logger.error(`Error running migrations:${error}`, {
-        context: 'DatabaseModule',
-        error: error,
-      });
-      process.exit(1);
-    }
-  }
+export class DatabaseModule {
 }
--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,20 +1,90 @@
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
-import { Knex } from 'knex';
+import { Injectable, Inject, OnModuleInit } from '@nestjs/common';
+import { knex, Knex } from 'knex';
+import Postgrator from 'postgrator';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'winston';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import * as process from 'node:process';
+import { getKnexConfig } from './knexfile';
 
 @Injectable()
 export class DatabaseService implements OnModuleInit {
-  constructor(@Inject('KnexConnection') private readonly knex: Knex) {}
+  private readonly knexConnection: Knex;
 
-  async onModuleInit(): Promise<any> {
-    await this.runMigrations();
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {
+    const dbConfig = getKnexConfig(configService);
+    this.knexConnection = knex(dbConfig);
   }
 
-  async runMigrations() {
+  async onModuleInit() {
     try {
-      await this.knex.migrate.latest();
-      console.log('Migrations are up to date');
+      await this.knexConnection.raw('SELECT 1+1 AS result');
+      this.logger.info('Database connection established successfully', {
+        context: 'DatabaseService',
+      });
+
+      this.knexConnection.on('error', (err) =>
+        this.logger.error('Database error', {
+          error: err,
+          context: 'DatabaseService',
+        }),
+      );
+
+      const dbConfig = getKnexConfig(this.configService);
+      const postgrator = new Postgrator({
+        migrationPattern: `${process.cwd()}/migrations/*`,
+        driver: 'pg',
+        database: (dbConfig.connection as any).database,
+        schemaTable: 'schema_migrations',
+        execQuery: (query) => this.knexConnection.raw(query),
+      });
+
+      postgrator.on('validation-started', (migration) =>
+        this.logger.info(`Validating ${migration.filename}`, {
+          context: 'DatabaseService',
+        }),
+      );
+      postgrator.on('validation-finished', (migration) =>
+        this.logger.info(`Validated ${migration.filename}`, {
+          context: 'DatabaseService',
+        }),
+      );
+      postgrator.on('migration-started', (migration) =>
+        this.logger.info(`Migrating ${migration.filename}`, {
+          context: 'DatabaseService',
+        }),
+      );
+      postgrator.on('migration-finished', (migration) =>
+        this.logger.info(`Migrated ${migration.filename}`, {
+          context: 'DatabaseService',
+        }),
+      );
+
+      try {
+        const migrations = await postgrator.migrate();
+        this.logger.info(`Ran ${migrations.length} migrations`, {
+          context: 'DatabaseService',
+        });
+      } catch (error) {
+        this.logger.error(`Error running migrations:${error}`, {
+          context: 'DatabaseService',
+          error: error,
+        });
+        process.exit(1);
+      }
     } catch (error) {
-      console.error('Migration failed:', error);
+      this.logger.error('Failed to connect to the database', {
+        error,
+        context: 'DatabaseService',
+      });
+      throw error;
     }
+  }
+
+  getKnexConnection(): Knex {
+    return this.knexConnection;
   }
 }
